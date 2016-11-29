2dcb08711d546367ee940048583e706dd138e7f0
psylh5 <psylh5@exmail.nottingham.ac.uk>
2016-11-29 20:44:49 +0000

    This is to fetch film summaries from imdb and stores the outputs in a text file

--- imdbStorylineGet.py
+++ imdbStorylineGet.py
@@ -5,33 +5,53 @@ import os
 
 #XPATH Links
 #Storyline = //*[@id="titleStoryLine"]/div[1]/p/text()
-#Langugae = //*[@id="titleDetails"]/div[3]/a
+#Langugae = //*[@id="titleDetails"]/div[3]/a            done
 #Media type = //*[@id="title-overview-widget"]/div[2]/div[2]/div/div[2]/div[2]/div/a[3]/text()
 #Adult content = //*[@id="title-overview-widget"]/div[2]/div[2]/div/div[2]/div[2]/div/a[1]/span
-#Page exists = //*[@id="error"]/div[1]/text()[1]
+#Page exists = //*[@id="error"]/div[1]/text()[1]        done
 
 default_web_url = "http://www.imdb.com/title/tt"    #Base website URL to fetch from
-counter = 0     #Counter to keep track on how many are done
-page_number_int = 109830     #Page number to search
+counter = 3     #Counter to keep track on how many are done
+page_number_int = 100000   #Page number to search
 
 #Create Directory if it doesn't exist for the out put files
 if not os.path.exists("FilmSummary"):
     os.makedirs("FilmSummary")
 
-#while (count <= 10):
+while (counter <= 10000):
+    page_number = "{0:0>7}".format(page_number_int)         #Page number to search in the correct format
+    web_url = default_web_url + str(page_number)            #The website URL
+    page = requests.get(web_url)                            #To retrive the web page
+    tree = html.fromstring(page.content)                    #Save the results of the web page in true
+    incorrect_page = str(tree.xpath('//*[@id="error"]/div[1]/text()[1]'))    #Find out if the page requested is valid
 
-page_number = "{0:0>7}".format(page_number_int)
-    #The website URL
-web_url = default_web_url + str(page_number)
-    #To retrive the web page
-page = requests.get(web_url)
-    #Save the results of the web page in true
-tree = html.fromstring(page.content)
-    #The xpath for the storyline from webpage
-storyline = tree.xpath('//*[@id="titleStoryLine"]/div[1]/p/text()')
-    #
+    if incorrect_page == '[]':      #if the page has content
+        language = str(tree.xpath('//*[@id="titleDetails"]/div[3]/a/text()'))
+        language = language.strip("[ ] ' ' , ` `")
+        if "English" in language:       #if the film is English
+            media_type = str(tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[2]/div[2]/div/a[3]/text()'))
+            media_type = media_type.strip("[ ] ' ' , ` `")
+            if media_type != '':        #Check the page is for a film
+                genres = str(tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[2]/div[2]/div/a[1]/span/text()'))
+                genres += str(tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[2]/div[2]/div/a[2]/span/text()'))
+                genres += str(tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[2]/div[2]/div/a[3]/span/text()'))
+                if "Adult" not in genres:       #Make sure the film is not a porno
+                    storyline = str(tree.xpath('//*[@id="titleStoryLine"]/div[1]/p/text()'))    #The xpath for the storyline from webpage
+                    storyline = storyline.strip("[ ] ' ' , ` `")
+                    #Save the resutls in a txt file
+                    f = open('FilmSummary/' + str(counter) +'.txt', 'w')
+                    f.write(storyline)
+                    f.close()
+                    counter += 1
+                    print (counter)
+                    print ("Valid")
+    #             else:
+    #                 print ("NOT VALID GENRE\n")
+    #         else:
+    #             print ("NOT VALID MEDIA TYPE\n")
+    #     else:
+    #         print ("NOT VALID LANGUAGE\n" + language)
+    # else:
+    #     print ("NOT VALID WEB PAGE\n")
 
-#Save the resutls in a txt file
-f = open('FilmSummary/' + str(counter) +'.txt', 'w')
-f.write(str(storyline))
-f.close()
+    page_number_int += 1
