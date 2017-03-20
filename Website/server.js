7ace2c69fcd47a87a9341df8ebbe60d04c97b8c7
psyrp1 <psyrp1@exmail.nottingham.ac.uk>
2017-03-20 01:35:14 +0000

    database connected to server

--- Website/server.js
+++ Website/server.js
@@ -1,8 +1,10 @@
 var express = require('express');  
+var mongodb = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var app = express();  
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
+var url = 'mongodb://localhost:27017/gifs';
 
 app.use(express.static('public'));
 
@@ -15,29 +17,24 @@ io.on('connection', function(socket){
     console.log('a user connected');
 
     socket.on('event', function(data) {
-        var myJSON = JSON.stringify(data);
-        console.log(myJSON);
+        MongoClient.connect(url, function(err, db) {
+            console.log("Connected to database");
+
+            insertDocuments(db, data, function() {
+                db.close();
+            });
+        });
     });
 });
 
-var insertDocuments = function(db, callback) {
+var insertDocuments = function(db, data, callback) {
     var collection = db.collection('gifs');
     // Insert some documents 
-    collection.insertMany([
-        {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-        console.log("Inserted 3 documents into the document collection");
+    collection.insert(data, function(err, result) {
+        console.log("Database entry made");
         callback(result);
     });
 }
 
-var url = 'mongodb://localhost:27017/';
-MongoClient.connect(url, function(err, db) {
-    console.log("Connected correctly to server");
-
-    insertDocuments(db, function() {
-        db.close();
-    });
-});
 
 server.listen(8000);
