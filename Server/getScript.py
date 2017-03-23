d54621c1d920f92ec4b430cec11305c2ead13ac8
Tom <psytjha@nottingham.ac.uk>
2017-03-23 12:53:04 +0000

    Added getTree function to the web crawler, to generalise the web crawling

--- Server/getScript.py
+++ Server/getScript.py
@@ -1,17 +1,35 @@
 from lxml import html
 import requests
 
+def getTree(URL):
+    page = requests.get(URL)
+    tree = html.fromstring(page.content)
+    return tree
+
 baseURL = "http://www.imsdb.com/all%20scripts"
 
 query = 'Force Awakens'
 
-page = requests.get(baseURL)
-tree = html.fromstring(page.content)
-#filmLinks = tree.xpath("//*[starts-with(local-name(), 'p')]")
-filmLinks = tree.xpath("//a[contains(., '')]")
+#Goes to the film
 
+tree = getTree(baseURL)
+#page = requests.get(baseURL)
+#tree = html.fromstring(page.content)
+#filmLinks = tree.xpath("//*[starts-with(local-name(), 'p')]")
+filmLinks = tree.xpath("//a[contains(., 'Avengers')]/@href")
+filmNames = tree.xpath("//a[contains(., 'Avengers')]")
 
 #print(filmLinks)
+for index, a in enumerate(filmLinks):
+    print(index, a)
 
-for index, p in enumerate(filmLinks):
-    print(index, p.text)
+for index, a in enumerate(filmNames):
+    print(index, a.text)
+
+#for index, a in enumerate(filmLinks):
+#    print(index, a)
+
+print(filmLinks[0], filmNames[0].text)
+
+
+#http://www.imsdb.com/Movie%20Scripts/Avengers,%20The%20(2012)%20Script.html
