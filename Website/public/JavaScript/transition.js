ae685704399df6c291dedca5f118362448823dc8
psydb3 <psydb3@exmail.nottingham.ac.uk>
2017-04-03 23:20:58 +0100

    Integration of quiz with existing system

--- Website/public/JavaScript/transition.js
+++ Website/public/JavaScript/transition.js
@@ -1,11 +1,29 @@
+var searchExpanded = false;
+var quizExpanded = false;
+var searchResultsExpanded = false;
 
+//deals with issues involved with single page website
+//handles browser back button presses
+window.onpopstate = function(event) { 
+	if (searchExpanded && !searchResultsExpanded)
+		RecedeSearch();
+	else if (searchResultsExpanded)
+		RecedeGif();
+	else if (quizExpanded)
+		RecedeQuiz();
+}
 function ExpandSearch() { //expand the home page search button to the full page content
+	if (quizExpanded) //prevents user expanding both pages when pressed fast enough
+		return;
+	searchExpanded = true;
+	history.pushState({page: "Search"}, "search", "#search");
 	unload = document.getElementById("search-load");
 	load = document.getElementById("search-full");
 	unload.style.opacity = 0;
 	DelayStateTransitions(unload, load, true, 0);
 }
 function RecedeSearch() { //recede the full page content back to the home page
+	searchExpanded = false;
 	unload = document.getElementById("search-full");
 	load = document.getElementById("search-load");
 	unload.style.opacity = 0;
@@ -13,29 +31,40 @@ function RecedeSearch() { //recede the full page content back to the home page
 }
 
 function ExpandQuiz() {//expand the home page play button to the full quiz content
+	if (searchExpanded)
+		return;
+	quizExpanded = true;
+	history.pushState({page: "Quiz"}, "Quiz", '#quiz');
 	unload = document.getElementById("quiz-load");
 	load = document.getElementById("quiz-full");
 	unload.style.opacity = 0;
 	DelayStateTransitions(unload, load, true, 1);
+	//Starts the quiz
 	showGif();
 	nextquestion();
 }
 function RecedeQuiz() {
+	quizExpanded = false;
 	unload = document.getElementById("quiz-full");
 	load = document.getElementById("quiz-load");
 	unload.style.opacity = 0;
-	DelayStateTransitions(unload, load, false, 1);
+	DelayStateTransitions(unload, load, false, 1);	
+	//Reset the quiz
 	current = -1;
 	score = 0;	
 }
 
 function ExpandGif() { //transitions from search page to results page
+	//document.body.style.cursor = "wait";
+	searchResultsExpanded = true;
+	history.pushState({page: "Search-Results"}, "Search Results", "#results");
 	unload = document.getElementById("search-full");
 	load = document.getElementById("search-gif");
 	unload.style.opacity = 0;
 	DelayStateTransitions(unload, load, -1, 0);
 }
 function RecedeGif() { //transitions from results page back to search page
+	searchResultsExpanded = false;
 	document.getElementById("film-name").value = '';
 	unload = document.getElementById("search-gif");
 	load = document.getElementById("search-full");
