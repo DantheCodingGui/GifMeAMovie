eaf2dcdc864172317e6fb84937b4da3333de3a82
Tom <psytjha@nottingham.ac.uk>
2017-04-04 16:40:25 +0100

    minor fix to giphy.js

--- Website/public/JavaScript/giphy.js
+++ Website/public/JavaScript/giphy.js
@@ -1,19 +1,19 @@
 //giph.js handles the giphy api
 //input = 10 word strings stored in array
 //output = 10 gifs that are played on repeat
-console.log("testing node");
+var reviewSubmitted = 0;
 var myarray;//array that stores words- usually 10
 var q; //q represents the string search query for the giphy API
 var i = 0; // counter
 var mp4Link = []; //array to store the mp4 link of the gif to help with future implementations
 
 function callGetGIF() {
-	myarray = idf(document.getElementById("summary").value);//accessing html field (user typed summary)
-                                                                //runs string through idf() from tfidf.js
-	q = myarray[0];                                     
-	document.getElementById("summary").remove();
-	document.getElementById("submit-sum").remove();
-	getGIF();
+    myarray = idf(document.getElementById("film-name").value);//accessing html field (user typed summary)
+    //runs string through idf() from tfidf.js
+    q = myarray[0];                                     
+	document.getElementById("upvote").display = "block";
+	document.getElementById("downvote").display = "block";
+    getGIF();
 }
 
 function getGIF() {
@@ -22,16 +22,16 @@ function getGIF() {
     request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true); //use of giphy api key
 
     request.onload = function getGif() {
-       if (i < myarray.length) { //increments array index
-           i++;
-        q = myarray[i]; //re-assign query
-       }
+        if (i < myarray.length) { //increments array index
+            i++;
+            q = myarray[i]; //re-assign query
+        }
 
         if (i == myarray.length) { //condition to restart the loop if the last gif has been shown
             i = 0;
             q = myarray[i];
         }
-        
+
         console.log(q); //for seeing the query
 
         if (request.status >= 200 && request.status < 400) { //in other words, if the request was a success
@@ -50,4 +50,14 @@ function getGIF() {
     };
 
     request.send(); //sends request at thet end
+}
+
+function upVote() {
+var socket = io.connect('http://rishi.doubletrouble.co:8000'); 
+socket.emit('event', {title: 'unkown', score: 'upvote', url: mp4Link[i], word: q});
+}
+
+function downVote() {
+var socket = io.connect('http://rishi.doubletrouble.co:8000'); 
+socket.emit('event', {title: 'unkown', score: 'downvote' , url: mp4Link[i], word: q});
 }
