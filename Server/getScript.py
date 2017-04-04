158f3d84a7c560121aa3d3797343f3dbabb821d3
Tom <psytjha@nottingham.ac.uk>
2017-04-04 22:03:12 +0100

    Fixed bug where no movie scripts can be found, due to genre tags messing up indexing

--- Server/getScript.py
+++ Server/getScript.py
@@ -5,7 +5,7 @@ def getTree(URL):
     page = requests.get(URL)
     tree = html.fromstring(page.content)
     return tree
-
+0
 def saveToFile(script, filmName):
     f = open("FilmScript/" + str(filmName) + ".txt","w")
     f.write(str(script))
@@ -14,10 +14,17 @@ def saveToFile(script, filmName):
 def getscript(filmURL, filmName):
     tree = getTree(filmURL)
     page = tree.xpath("//a[contains(., '')]/@href")
-    scriptTree = getTree("http://www.imsdb.com"+page[67])
-    scriptPage = scriptTree.xpath("//pre/text()[normalize-space(/)]")
-#The option below will write to file
-    saveToFile(scriptPage, filmName)
+
+    i = 65  #all movie script links are between 65 and 73
+    while i < 73:
+        if page[i][1:7] == "script": break  #checks to see if link tag is found.
+        i=i+1
+    if i != 73: #film with no script will have a page[i], where i == 73, thus not being written to a file.
+        #Gets the movie script.
+        scriptTree = getTree("http://www.imsdb.com"+page[i])
+        scriptPage = scriptTree.xpath("//pre/text()")
+
+        saveToFile(scriptPage, filmName)    #call save to file function.
 
 #This URL gets the crawler to a page with links to all of the scripts
 baseURL = "http://www.imsdb.com/all%20scripts"
@@ -29,5 +36,7 @@ filmNames = tree.xpath("//a[contains(., '')]/text()")
 baseURL = "http://www.imsdb.com"
 
 #current crawler is for single element
-#for i in range (64 , 1229):
-getscript(baseURL+filmLinks[500], filmNames[500])   #uncomment line 33 and change 500 to i to fix
+for i in range (64 , 1229): # 64, 1229):
+    getscript(baseURL+filmLinks[i+4], filmNames[i])   #uncomment line 33 and change 500 to i to fix
+    #print(filmNames[i])
+    #print(filmLinks[i+4])
