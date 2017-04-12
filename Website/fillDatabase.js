0c0637659c63380f578fd3ae9cf9fe234994d70f
psyrp1 <psyrp1@exmail.nottingham.ac.uk>
2017-04-12 18:16:02 +0000

    made script to fill database with all films

---
+++ Website/fillDatabase.js
@@ -0,0 +1,25 @@
+var mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/gifs';
+var fs = require("fs");
+var content = fs.readFileSync("FilmAndWords.json");
+
+var jSon = JSON.parse(content); //parse JSON
+
+console.log("Output Content : \n"+ content);
+
+MongoClient.connect(url, function(err, db) {
+    console.log("Connected to database");
+    insertDocuments(db, jSon, function() {
+    db.close();
+    });
+});
+
+var insertDocuments = function(db, data, callback) {
+    var collection = db.collection('gifs');
+    // Insert some documents 
+    collection.insert(data, function(err, result) {
+    console.log("Database entry made");
+    callback(result);
+    });
+}
