a7bc34ade5aeacfc2649048d34966b0dbe411487
psyrp1 <psyrp1@exmail.nottingham.ac.uk>
2017-05-15 22:48:59 +0000

    site working properly with gifs now

--- Website/public/JavaScript/giphy.js
+++ Website/public/JavaScript/giphy.js
@@ -1,70 +1,70 @@
 //giph.js handles the giphy api
 //input = 10 word strings stored in array
 //output = 10 gifs that are played on repeat
-var socket = io.connect('http://rishi.doubletrouble.co:8000'); 
+var socket = io.connect('http://gifmeamovie.co.uk:8080'); 
 var reviewSubmitted = 0;
 var myarray = {};//array that stores words- usually 10
 var i = 0; // counter
 var mp4Link = []; //array to store the mp4 link of the gif to help with future implementations
 var q; //q represents the string search query for the giphy API
+var isQuiz = false;
 
 
 function callGetGIF() {
     // myarray = idf(filmName);//accessing html field (user typed summary)
-    if (!isQuiz){
-        var filmName = document.getElementById("film-name").value;
+    var filmName = document.getElementById("film-name").value;
 
-    socket.emit('queryDB', {title: filmName});
+    socket.emit('searchQueryDB', {title: filmName});
 
-    socket.on('array', function(data) {
+    socket.on('searchArray', function(data) {
         // console.log(data.Words);
         myarray = data.Words;
     })
 
 
-    q = myarray[0];                                     
+    q = myarray[0];
     document.getElementById("upvote").display = "block";
     document.getElementById("downvote").display = "block";
     getGIF(q);
-    }
 }
 
 function getGIF() {
 
-    request = new XMLHttpRequest; //allows for loading url without having to refresh page
-    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true); //use of giphy api key
 
-    request.onload = function getGif() {
-        if (i < myarray.length) { //increments array index
-            i++;
-            q = myarray[i]; //re-assign query
-        }
+        request = new XMLHttpRequest; //allows for loading url without having to refresh page
+        request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true); //use of giphy api key
 
-        if (i == myarray.length) { //condition to restart the loop if the last gif has been shown
-            i = 0;
-            q = myarray[i];
-            getGIF();
-        }
+        request.onload = function getGif() {
+            if (i < myarray.length) { //increments array index
+                i++;
+                q = myarray[i]; //re-assign query
+            }
+
+            if (i == myarray.length) { //condition to restart the loop if the last gif has been shown
+                i = 0;
+                q = myarray[i];
+                getGIF();
+            }
 
 
-        console.log(q); //for seeing the query
+            console.log(q); //for seeing the query
 
-        if (request.status >= 200 && request.status < 400) { //in other words, if the request was a success
-            data = JSON.parse(request.responseText).data.image_original_url;
-            mp4Link.push(data); //adding mp4 link to array
-            // document.getElementById("giphyme").innerHTML = '<center><img onclick="getGIF()" src = "'+data+'"  title="GIF via Giphy"></center>';
-            // if (mp4Link.length = myarray.length) //make so that stores links so doesnt need to show a different summary
-            document.getElementById("searchgif").innerHTML = '<center><image height="400px" width = "auto" src="'+data+'" autoplay onended="getGIF()"></image>';//show mp4
-        } else {
-            console.log('reached giphy, but API returned an error'); //error handling
-        }
-    };
+            if (request.status >= 200 && request.status < 400) { //in other words, if the request was a success
+                data = JSON.parse(request.responseText).data.image_original_url;
+                mp4Link.push(data); //adding mp4 link to array
+                // document.getElementById("giphyme").innerHTML = '<center><img onclick="getGIF()" src = "'+data+'"  title="GIF via Giphy"></center>';
+                // if (mp4Link.length = myarray.length) //make so that stores links so doesnt need to show a different summary
+                document.getElementById("searchgif").innerHTML = '<center><image height="400px" width = "auto" src="'+data+'" autoplay onended="getGIF()"></image>';//show mp4
+            } else {
+                console.log('reached giphy, but API returned an error'); //error handling
+            }
+        };
 
-    request.onerror = function() {
-        console.log('connection error');
-    };
+        request.onerror = function() {
+            console.log('connection error');
+        };
 
-    request.send(); //sends request at thet end
-}
+        request.send(); //sends request at thet end
+    }
 setInterval(getGIF, 4000);
 
--- Website/public/JavaScript/quiztest.js
+++ Website/public/JavaScript/quiztest.js
@@ -1,21 +1,19 @@
 var wordArray = {} //array that contains key words for relevant film
-var socket = io.connect('http://rishi.doubletrouble.co:8000'); //define socket connecing to domain on port 8000
+var socket = io.connect('http://http://gifmeamovie.co.uk:8080'); //define socket connecing to domain on port 8000
 var popularFilms = ["The Dark Knight", "Gladiator", "Toy Story 3", "Forrest Gump", "The Prestige", "Amadeus", "Jurassic Park", "Jaws"]; //select films for quiz
 var i = 0; //counter
 var q;
-var isQuiz = false;
-
 
 function randomFilm() {
 
 
-    var randomNum = Math.floor((Math.random() * popularFilms.length - 1) + 0) //
-    var filmName = popularFilms[randomNum]
+    var randomNum = Math.floor((Math.random() * popularFilms.length - 1) + 0) //pick a random element based on the size of the film array
+    var filmName = popularFilms[randomNum] //obtain one film name from the array
 
-    socket.emit('queryDB', {title: filmName});
+    socket.emit('quizQueryDB', {title: filmName});//send the film name from client to server
 
 
-    socket.on('array', function(data) {
+    socket.on('quizArray', function(data) { //receive processed film in the form of an array of words
         wordArray = data.Words;
         if(wordArray != 0) {
             q = wordArray[0];
@@ -29,10 +27,10 @@ function quizGIF() {
 
 
 
-    request = new XMLHttpRequest; //allows for loading url without having to refresh page
-    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + q, true); //use of giphy api key
+    request2 = new XMLHttpRequest; //allows for loading url without having to refresh page
+    request2.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + q, true); //use of giphy api key
 
-    request.onload = function quizGIF() {
+    request2.onload = function quizGIF() {
         if (i < wordArray.length) { //increments array index
             i++;
             q = wordArray[i]; //re-assign query
@@ -44,13 +42,12 @@ function quizGIF() {
             quizGIF();
         }
 
-        // console.log(request.status);
-
+        // console.log(request2.status);
 
         console.log(q); //for seeing the query
 
-        if (request.status >= 200 && request.status < 400) { //in other words, if the request was a success
-            data = JSON.parse(request.responseText).data.image_original_url;
+        if (request2.status >= 200 && request2.status < 400) { //in other words, if the request2 was a success
+            data = JSON.parse(request2.responseText).data.image_original_url;
             // document.getElementById("giphyme").innerHTML = '<center><img onclick="getGIF()" src = "'+data+'"  title="GIF via Giphy"></center>';
             // if (mp4Link.length = myarray.length) //make so that stores links so doesnt need to show a different summary
             document.getElementById("quizgif").innerHTML = '<center><image height="400px" width="auto" src="'+data+'" autoplay onended="quizGIF()"></image>';//show mp4
@@ -60,10 +57,10 @@ function quizGIF() {
         }
     };
 
-    request.onerror = function() {
+    request2.onerror = function() {
         console.log('connection error');
     };
 
-    request.send(); //sends request at thet end
+    request2.send(); //sends request2 at thet end
 }
 setInterval(quizGIF,4000);
